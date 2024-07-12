2a25ad1c67fde8af67ac406836da2d0f40c920a2
swd <swdchw@gmail.com>
2024-07-12 20:38:01 +0500

    base

--- src/AudioPlayer.API/Features/Extensions.cs
+++ src/AudioPlayer.API/Features/Extensions.cs
@@ -4,7 +4,56 @@
 
 namespace AudioPlayer.API.Features;
 
+using Exiled.API.Features;
+using NVorbis;
+
+/// <summary>
+/// A collection of API methods.
+/// </summary>
 public static class Extensions
 {
+    /// <summary>
+    /// Gets path to audio file root directory. Leads to EXILED root directory (EXILED/Audio).
+    /// </summary>
+    public static string FolderPath { get; } = Path.Combine(Paths.Exiled, "Audio", "track.ogg");
 
+    /// <summary>
+    /// Checks the path for playable track.
+    /// </summary>
+    /// <param name="path">Path to file.</param>
+    /// <returns>Is file exists and ready to be played.</returns>
+    public static bool CheckTrack(string path)
+    {
+        if (!Directory.Exists(FolderPath))
+        {
+            Directory.CreateDirectory(FolderPath);
+        }
+
+        if (!File.Exists(path))
+        {
+            return false;
+        }
+
+        if (Path.GetExtension(path) != ".ogg")
+        {
+            VorbisReader vorbisReader = new(path);
+
+            Log.Info(vorbisReader.Channels);
+            Log.Info(vorbisReader.NominalBitrate);
+
+            return ConvertToOgg(path);
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Convert file to playable track.
+    /// </summary>
+    /// <param name="path">Path to file.</param>
+    /// <returns>Is file can be converted and ready to be played.</returns>
+    public static bool ConvertToOgg(string path)
+    {
+        return true;
+    }
 }
