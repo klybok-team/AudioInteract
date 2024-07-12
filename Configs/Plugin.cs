59f02b2da6f188e1641955dc2708aaee4b10fe7d
swd <swdchw@gmail.com>
2024-07-12 19:17:06 +0500

    Init

---
+++ Configs/Plugin.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Plugin.cs" company="Klybok Team">
+// Copyright (c) Klybok Team. All rights reserved.
+// </copyright>
+
+namespace AudioPlayer.Configs;
+
+using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+/// <summary/>
+public sealed class Plugin : IConfig
+{
+    /// <summary/>
+    [Description("Indicates plugin is enabled or not")]
+    public bool IsEnabled { get; set; } = true;
+
+    /// <summary/>
+    [Description("Indicates debug mode is enabled or not")]
+    public bool Debug { get; set; } = true;
+}
---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="Plugin.cs" company="Klybok Team">
+// Copyright (c) Klybok Team. All rights reserved.
+// </copyright>
+
+namespace AudioPlayer;
+
+using Exiled.API.Features;
+using HarmonyLib;
+
+/// <inheritdoc/>
+public class Plugin : Exiled.API.Features.Plugin<Configs.Plugin>
+{
+    /// <summary/>
+    public const string HarmonyID = $"AudioPlayer - Klybok Team";
+
+    /// <summary/>
+    public static Harmony? Harmony { get; private set; }
+
+    /// <inheritdoc/>
+    public override string Name => "AudioPlayer";
+
+    /// <inheritdoc/>
+    public override string Author => "Klybok Team";
+
+    /// <inheritdoc/>
+    public override Version Version => new(1, 0, 0);
+
+    /// <inheritdoc/>
+    public override void OnEnabled()
+    {
+        Harmony = new Harmony(HarmonyID);
+        Harmony.PatchAll();
+
+        base.OnEnabled();
+    }
+
+    /// <inheritdoc/>
+    public override void OnDisabled()
+    {
+        Harmony?.UnpatchAll(HarmonyID);
+        Harmony = null;
+
+        base.OnDisabled();
+    }
+}
